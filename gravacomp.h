fd6acef6107ae79ed266daf3a13e17b3483c088d
ricleta <47068518+ricleta@users.noreply.github.com>
2022-05-09 12:09:03 -0300

    Update gravacomp.h

--- gravacomp.h
+++ gravacomp.h
@@ -1,3 +1,3 @@
 #include <stdio.h>
-int gravacomp (int nstructs, void* valores, char* campos, FILE* arquivo);
-void mostracomp (FILE* arquivo);
+void mostracomp(FILE *arquivo);
+int gravacomp (int nstructs, void* valores, char* descritor, FILE* arquivo);
